4cc9c424352de93b53ea573487eddf35c5a97868
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-04 16:05:31 +0100

    obsidian-auto-push : 2026-02-04 16:05:31

--- IN360/TD/TD1/Chronometre.java
+++ IN360/TD/TD1/Chronometre.java
@@ -1,3 +1,55 @@
 public class Chronometre {
-    
+    private int heures;
+    private int minutes;
+    private int secondes;
+
+
+
+    public Chronometre(){
+        heures = 0;
+        minutes = 0;
+        secondes = 0;
+    }
+
+    /* Getters */
+
+    public int getHeures(){
+        return heures;
+    }
+    public int getMinutes(){
+        return minutes;
+    }
+    public int getSecondes(){
+        return secondes;
+    }
+
+    /* Setters */
+
+    public void setHeures(int heure){
+        heures = heure;
+    }
+    public void setMinutes(int minute){
+        minutes = minute;
+    }
+    public void setSecondes(int seconde){
+        secondes = seconde;
+    }
+
+    /* Rebours */
+
+    public void rebours(){
+        if (secondes == 0) {
+            if (minutes == 0) {
+                
+            }
+            else{
+                
+            }
+        }
+        else{
+            secondes--;
+        }
+    }
+
+
 }
