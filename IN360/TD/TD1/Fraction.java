6a245cac54cb9230bc656a48a9a6d98b59460700
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-04 17:05:34 +0100

    obsidian-auto-push : 2026-02-04 17:05:34

--- IN360/TD/TD1/Fraction.java
+++ IN360/TD/TD1/Fraction.java
@@ -1,3 +1,22 @@
 public class Fraction {
-    
+    private int num;
+    private int den;
+
+    public Fraction(int num, int den){
+        if (den == 0) {
+            System.out.println("IMPOSSIBLE DE DIVISER PAR 0 MALHEUREUX");
+            return;
+        }
+        else{
+            this.num = num;
+            this.den = den;
+        }
+    }
+
+    public void add(int x){
+        
+    }
+
+
+
 }
