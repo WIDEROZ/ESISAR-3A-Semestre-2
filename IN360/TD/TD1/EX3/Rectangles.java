2b1b0cbffe69c98a8789efc353f422b5542f7b42
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-10 16:00:21 +0100

    obsidian-auto-push : 2026-02-10 16:00:21

--- IN360/TD/TD1/EX3/Rectangles.java
+++ IN360/TD/TD1/EX3/Rectangles.java
@@ -1,6 +1,25 @@
 package EX3;
 
 public class Rectangles {
+
+    private Point P1;
+    private Point P2;
+
+    public Rectangles(double l1, double l2, double l3, double l4){
+        P1.setPoint(0, 0);
+
+
+    }
+
+    public Rectangles(Point a, Point b){
+        
+
+    }
+
     
-    
+
+    public Rectangles(Point a, double l1, double l2){
+
+    }
+
 }
