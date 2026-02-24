997cf43ef3cb7ca084dd99da20535b636e4d4e3b
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-24 10:57:10 +0100

    obsidian-auto-push : 2026-02-24 10:57:10

--- IN360/TP/TP1/in360/src/partie2/Ex1.java
+++ IN360/TP/TP1/in360/src/partie2/Ex1.java
@@ -6,21 +6,35 @@ public class Ex1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Nombre de notes : ")
-		int N = sc.nextInt();
+		System.out.print("Nombre de notes : ");
+		int n = sc.nextInt();
 		System.out.println();
 		
-		int[] tab;
-		tab = new int[N];
+		int[] notes;
+		notes = new int[n];
+		int[] coefs;
+		coefs = new int[n];
 		
-		while(N > 0) {
-			
-			
-			
-			
-			
-			
-			N--;
+		int moyenne = 0;
+		
+		int k = 0;
+		while(k < n) {
+			System.out.print("Entrez la note numéro : " + k);
+			notes[k] = sc.nextInt();
+			System.out.println();
+			k--;
 		}
+		k = 0;
+		while(k < n) {
+			System.out.print("Entrez le coef numéro : " + k);
+			coefs[k] = sc.nextInt();
+			System.out.println();
+			k--;
+		}
+		k = 0;
+		while(k < n) {
+			moyenne+= notes[k]*coefs[k];
+		}
+		
 	}
 }
