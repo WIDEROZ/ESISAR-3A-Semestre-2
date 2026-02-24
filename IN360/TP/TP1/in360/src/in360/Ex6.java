2d460ff39a04322c709926a9f899f977cb79e434
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-24 09:58:54 +0100

    obsidian-auto-push : 2026-02-24 09:58:54

--- IN360/TP/TP1/in360/src/in360/Ex6.java
+++ IN360/TP/TP1/in360/src/in360/Ex6.java
@@ -9,8 +9,33 @@ public class Ex6 {
 		int n_10c = 0;
 
 
-		while(n_2c != ) {
-			if(n_2c >= )
+		while((n_2c != 50) && (n_5c != 20) && (n_10c != 10)) {
+			if(n_2c > 50) {
+				n_2c = 0;
+			}
+			if(n_5c > 20) {
+				n_5c = 0;
+			}
+			if(n_10c > 10) {
+				n_10c = 0;
+			}
+			
+			sum = n_2c * 2 + n_5c*5 + n_10c*10;
+			if(sum == 100) {
+				System.out.print("1€ = ");
+				if(n_2c !=0) {
+					System.out.print(n_2c + " X 2c");
+				}
+				if(n_5c != 0) {
+					System.out.print(" + " + n_5c + " X 5c");
+				}
+				if(n_10c != 0) {
+					System.out.print(" + " + n_10c + " X 10c");
+				}
+			}
+			n_2c++;
+			n_5c++;
+			n_10c++;
 		}
 	}
 }
