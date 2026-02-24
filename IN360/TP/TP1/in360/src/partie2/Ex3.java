07d83cc24b314b0c3263548098643ed5ab37b6d9
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-24 11:27:26 +0100

    obsidian-auto-push : 2026-02-24 11:27:26

--- IN360/TP/TP1/in360/src/partie2/Ex3.java
+++ IN360/TP/TP1/in360/src/partie2/Ex3.java
@@ -4,8 +4,29 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ex3 {
-	ArrayList<Float> notes= new ArrayList<>();
-	Scanner sc = new Scanner(System.in);
-	
-	
+	public static void main(String[] args) {
+		ArrayList<Float> notes= new ArrayList<>();
+		ArrayList<Float> coefs= new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Saisissez la note (-1 pour finir) : ");
+		float scan = sc.nextFloat();
+		
+		while(scan != -1) {
+			notes.add(scan);
+			System.out.println("Saisissez la note (-1 pour finir) : ");
+			scan = sc.nextFloat();
+		}
+		System.out.println("Saisissez le coeff (-1 pour finir) : ");
+		scan = sc.nextFloat();
+		while(scan != -1) {
+			coefs.add(scan);
+			System.out.println("Saisissez le coeff (-1 pour finir) : ");
+			scan = sc.nextFloat();
+		}
+		
+		
+		
+		
+	}
 }
