da09fbcd7fb70747a844acbd13a6760dcabeaa12
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-24 11:17:21 +0100

    obsidian-auto-push : 2026-02-24 11:17:21

--- IN360/TP/TP1/in360/src/partie2/Ex2.java
+++ IN360/TP/TP1/in360/src/partie2/Ex2.java
@@ -12,12 +12,10 @@ public class Ex2 {
 		
 		float[] notes;
 		notes = new float[n];
-		float[] coefs;
-		coefs = new float[n];
 		
 		int k = 0;
 		while(k < n) {
-			System.out.print("Entrez la note numéro : " + k);
+			System.out.println("Entrez la note numéro : " + k);
 			notes[k] = sc.nextFloat();
 			System.out.println();
 			k++;
@@ -30,14 +28,30 @@ public class Ex2 {
 		float tmp = 0f;
 		while(exchange) {
 			k = 0;
+			exchange = false;
 			while(k < n-1) {
 				if(notes[k] > notes[k+1]) {
 					tmp = notes[k];
+					notes[k] = notes[k+1];
+					notes[k+1] = tmp;
+					exchange = true;
 				}
 				
 				k++;
 			}
 		}
+		
+		// Affichage des notes : 
+		
+		System.out.println("La note mini est de : " + notes[0]);
+		System.out.println("La note maxi est de : " + notes[n-1]);
+		
+		k = 0;
+		while(k <n) {
+			System.out.println("Note numéro " + k + " : " + notes[k]);
+			k++;
+		}
+		
 		
 	}
 }
---
+++ IN360/TP/TP1/in360/src/partie2/Ex3.java
@@ -0,0 +1,11 @@
+package partie2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex3 {
+	ArrayList<Float> notes= new ArrayList<>();
+	Scanner sc = new Scanner(System.in);
+	
+	
+}
