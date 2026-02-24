04098d43408fb0b2a8fd2045d47447b7955dfd81
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-24 09:48:49 +0100

    obsidian-auto-push : 2026-02-24 09:48:49

---
+++ IN360/TP/TP1/in360/src/in360/Ex6.java
@@ -0,0 +1,16 @@
+package in360;
+
+public class Ex6 {
+	public static void main(String[] args) {
+		System.out.println("1 € : ");
+		int sum = 0;
+		int n_2c = 0;
+		int n_5c = 0;
+		int n_10c = 0;
+
+
+		while(n_2c != ) {
+			if(n_2c >= )
+		}
+	}
+}
