a204d289696ea1aa744d052ce4d94ef8276847d4
WIDEROZ <115324601+WIDEROZ@users.noreply.github.com>
2026-02-24 11:47:37 +0100

    obsidian-auto-push : 2026-02-24 11:47:37

---
+++ IN360/TP/TP1/in360/src/partie2/Ex5.java
@@ -0,0 +1,25 @@
+package partie2;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		ArrayList<Int> nombres= new ArrayList<>();
+		
+		System.out.print("Donnez le nombre max de l'entier a examiner : ");
+		int n = sc.nextInt();
+		
+		int k = 0;
+		while(k < n) {
+			nombres.add(k+1);
+		}
+		k = 0;
+		while(k < nombres.size()) {
+			K 
+		}
+		
+	}
+}
